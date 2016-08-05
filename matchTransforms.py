9deeef7010875a00aeffc9d85482f481b07b0c47
illmill <millnug@me.com>
2016-08-05 00:39:50 -0400

    Update matchTransforms.py

--- matchTransforms.py
+++ matchTransforms.py
@@ -1,4 +1,31 @@
 
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Gist
+
+    @illmillrig
+
+1
+0
+
+    0
+
+illmillrig/matchTransforms
+Code
+Issues 0
+Pull requests 0
+Wiki
+Pulse
+Graphs
+Settings
+matchTransforms/matchTransforms.py
+6085494 5 hours ago
+@illmillrig illmillrig Add files via upload
+81 lines (62 sloc) 2.02 KB
+
 def getGlobalTransform(node, tfmPlug="worldMatrix"):
 	fn = om.MFnTransform(node)
 	plug = fn.findPlug(tfmPlug)
@@ -15,17 +42,24 @@ def getGlobalTransform(node, tfmPlug="worldMatrix"):
 def matchTransform(nodes, source, translate=True, rotate=True, scale=True, space=om.MSpace.kWorld, matchPivot=False):
 	# confirm/create MSelectionList of nodes
 
+	nodeList = []
+	
 	if isinstance(nodes, om.MSelectionList):
-		pass
-	elif isinstance(nodes, om.MObject) or isinstance(nodes, om.MDagPath):
-		sel = om.MSelectionList()
-		sel.add(nodes)
-		nodes = sel
-	elif isinstance(nodes, om.MDagPathArray):
-		sel = om.MSelectionList()
+		dagNode = om.MDagPath()
 		for i in xrange(nodes.length()):
-			sel.add(nodes[i])
-		nodes = sel
+			nodes.getDagPath(i, dagNode)
+			nodeList.append(dagNode)
+
+	elif isinstance(nodes, om.MObject): or isinstance(nodes, om.MDagPath):
+		nodeList.append(om.MDagPath.getAPathTo(nodes))
+
+	elif isinstance(nodes, om.MDagPath):
+		nodeList.append(nodes)
+
+	elif isinstance(nodes, om.MDagPathArray):
+		for i in xrange(nodes.length()):
+			nodeList.append(nodes[i])
+		
 
 	# get the proper matrix of source
 	if space == om.MSpace.kWorld:
@@ -40,20 +74,18 @@ def matchTransform(nodes, source, translate=True, rotate=True, scale=True, space
 	srcPivot = srcTfm.scalePivot(space)
 
 	# stupid MScriptUtil stuff
-	dagNode = om.MDagPath()
 	util = om.MScriptUtil()
 	util.createFromDouble(0.0, 0.0, 0.0)
 	scl = util.asDoublePtr()
 
 	fn = om.MFnTransform()
-	for i in xrange(nodes.length()):
-		nodes.getDagPath(i, dagNode)
-
+	for node in nodeList:
+	
 		if space == om.MSpace.kObject:
 			tfm = srcTfm
 		else:
 			# multiply the global scale and rotation by the nodes parent inverse world matrix to get local rot & scl
-			invParent = getGlobalTransform(dagNode, "parentInverseMatrix")
+			invParent = getGlobalTransform(node, "parentInverseMatrix")
 			tfm = om.MTransformationMatrix(srcTfm.asMatrix() * invParent.asMatrix())
 
 		# rotation
@@ -63,7 +95,7 @@ def matchTransform(nodes, source, translate=True, rotate=True, scale=True, space
 		tfm.getScale(scl, space)
 
 		# Set Transforms----------------------------
-		fn.setObject(dagNode)
+		fn.setObject(node)
 		# set Scaling
 		if scale:
 			fn.setScale(scl)
